5e6d45e2dca4938c00ce3258517a8d0e94efc3b3
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-07-11 22:04:05 +0300

    feat: implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { dirname, join } from 'path';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(4000);
 }
 bootstrap();
