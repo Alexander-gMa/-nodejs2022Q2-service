af22113736df3e8a5a6c752cf2e84ecb1917dded
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-07-16 17:29:27 +0300

    feat: add port with process.env

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 4000;
   const rootDirname = dirname(__dirname);
   const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
   const document = parse(DOC_API);
@@ -16,6 +17,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   SwaggerModule.setup('doc', app, document);
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
