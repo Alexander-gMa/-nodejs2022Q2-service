5f437a18e15d393191109401d0042104528ce638
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-07-22 00:20:10 +0300

    feat: add prisma module & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ArtistModule } from './artist/artist.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
 import { FavouriteModule } from './favourite/favourite.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { FavouriteModule } from './favourite/favourite.module';
     TrackModule,
     AlbumModule,
     FavouriteModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
